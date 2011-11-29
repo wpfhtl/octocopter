e7e6be7c152ae2fd175d472579c516177b8d343c
ben <ben@f4953187-ecf7-0310-90f3-8d0fedb0aae5>
2011-11-29 18:02:36 +0000

    forgot to add logreplay
    git-svn-id: https://politbuero.dyndns.org/svn/repos/promotion@1174 f4953187-ecf7-0310-90f3-8d0fedb0aae5

---
+++ koptercontrol/logreplay.cpp
@@ -0,0 +1,244 @@
+#include <QCoreApplication>
+#include "laserscanner.h"
+#include "sensorfuser.h"
+
+//#include <math.h>
+/*
+int KopterControl::signalFd[] = {0,0};
+
+void setupUnixSignalHandlers()
+{
+    // Set up all signals to call KopterControl::signalHandler()
+    struct sigaction intr, hup, term;
+
+    intr.sa_handler = KopterControl::signalHandler;
+    sigemptyset(&intr.sa_mask);
+    intr.sa_flags = 0;
+    intr.sa_flags |= SA_RESTART;
+
+    if(sigaction(SIGINT, &intr, 0) != 0) qFatal("Couldn't set up signal handler for SIGINT");
+
+    hup.sa_handler = KopterControl::signalHandler;
+    sigemptyset(&hup.sa_mask);
+    hup.sa_flags = 0;
+    hup.sa_flags |= SA_RESTART;
+
+    if(sigaction(SIGHUP, &hup, 0) != 0) qFatal("Couldn't set up signal handler for SIGHUP");
+
+    term.sa_handler = KopterControl::signalHandler;
+    sigemptyset(&term.sa_mask);
+    term.sa_flags |= SA_RESTART;
+
+    if(sigaction(SIGTERM, &term, 0) != 0) qFatal("Couldn't set up signal handler for SIGTERM");
+}
+
+KopterControl::KopterControl(int argc, char **argv) : QCoreApplication(argc, argv)
+{
+    // set up signal handling
+    if (::socketpair(AF_UNIX, SOCK_STREAM, 0, signalFd)) qFatal("Couldn't create INT socketpair");
+
+    snSignalPipe = new QSocketNotifier(signalFd[1], QSocketNotifier::Read, this);
+    connect(snSignalPipe, SIGNAL(activated(int)), SLOT(slotHandleSignal()));
+
+    QString networkInterface = "wlan0";
+    QString deviceCamera = "/dev/video0";
+    QString deviceSerialKopter = "/dev/ttyUSB0";
+    QString deviceSerialGpsCom = "/dev/ttyUSB1";
+    QString deviceSerialGpsUsb = "/dev/serial/by-id/usb-Septentrio_Septentrio_USB_Device-if00"; //"/dev/ttyACM0";
+    QString deviceSerialLaserScanner = "/dev/serial/by-id/usb-Hokuyo_Data_Flex_for_USB_URG-Series_USB_Driver-if00"; // "/dev/ttyACM1";
+
+    QStringList commandLine = arguments();
+
+    if(commandLine.lastIndexOf("-dc") != -1 && commandLine.size() > commandLine.lastIndexOf("-dc") + 1)
+    {
+        deviceCamera = commandLine.at(commandLine.lastIndexOf("-dc") + 1);
+    }
+
+    if(commandLine.lastIndexOf("-sk") != -1 && commandLine.size() > commandLine.lastIndexOf("-sk") + 1)
+    {
+        deviceSerialKopter = commandLine.at(commandLine.lastIndexOf("-sk") + 1);
+    }
+
+    if(commandLine.lastIndexOf("-sl") != -1 && commandLine.size() > commandLine.lastIndexOf("-sl") + 1)
+    {
+        deviceSerialLaserScanner = commandLine.at(commandLine.lastIndexOf("-sl") + 1);
+    }
+
+    if(commandLine.lastIndexOf("-sgu") != -1 && commandLine.size() > commandLine.lastIndexOf("-sgu") + 1)
+    {
+        deviceSerialGpsUsb = commandLine.at(commandLine.lastIndexOf("-sgu") + 1);
+    }
+
+    if(commandLine.lastIndexOf("-sgc") != -1 && commandLine.size() > commandLine.lastIndexOf("-sgc") + 1)
+    {
+        deviceSerialGpsCom = commandLine.at(commandLine.lastIndexOf("-sgc") + 1);
+    }
+
+    if(commandLine.lastIndexOf("-netiface") != -1 && commandLine.size() > commandLine.lastIndexOf("-netiface") + 1)
+    {
+        networkInterface = commandLine.at(commandLine.lastIndexOf("-netiface") + 1).toLower();
+    }
+
+    qDebug() << "KopterControl::KopterControl(): using serial ports: kopter" << deviceSerialKopter << "gps com" << deviceSerialGpsCom << "gps usb" << deviceSerialGpsUsb;
+    qDebug() << "KopterControl::KopterControl(): using laserscanner at" << deviceSerialLaserScanner;
+    qDebug() << "KopterControl::KopterControl(): reading RSSI at interface" << networkInterface;
+
+    mFlightController = new FlightController();
+    mLaserScanner = new LaserScanner(
+                deviceSerialLaserScanner,
+                Pose(
+                    QVector3D(      // Offset from Antenna to Laser Source. In Vehicle Reference Frame: Like OpenGL, red arm forward pointing to screen
+                        +0.09,      // From antenna positive is right to laser
+                        -0.38,      // From Antenna negative is down to laser
+                        -0.11),     // From Antenna negative forward to laser
+                    +000.0,         // No yawing
+                    -090.0,         // 90 deg pitched down
+                    +000.0,         // No rolling
+                    10             // Use 10 msec TOW, so that the relative pose is always older than whatever new pose coming in. Don't use 0, as that would be set to current TOW, which might be newer due to clock offsets.
+                    )
+                );
+    mSensorFuser = new SensorFuser(mLaserScanner);
+    mBaseConnection = new BaseConnection(networkInterface);
+    mKopter = new Kopter(deviceSerialKopter, this);
+    mGpsDevice = new GpsDevice(deviceSerialGpsUsb, deviceSerialGpsCom, this);
+
+//    mCamera = new Camera(deviceCamera, QSize(320, 240), QVector3D(), QQuaternion(), 15);
+//    mVisualOdometry = new VisualOdometry(mCamera);
+
+//    connect(mCamera, SIGNAL(imageReadyJpeg(QString,QSize,QVector3D,QQuaternion,const QByteArray*)), mBaseConnection, SLOT(slotNewCameraImage(QString,QSize,QVector3D,QQuaternion,const QByteArray*)));
+//    connect(mCamera, SIGNAL(imageReadyYCbCr(QString,QSize,QVector3D,QQuaternion,QByteArray)), mVisualOdometry, SLOT(slotProcessImage(QString,QSize,QVector3D,QQuaternion,QByteArray)));
+    connect(mLaserScanner, SIGNAL(message(LogImportance,QString,QString)), mBaseConnection, SLOT(slotNewLogMessage(LogImportance,QString,QString)));
+    connect(mKopter, SIGNAL(kopterStatus(quint32, qint16, float)), mBaseConnection, SLOT(slotNewVehicleStatus(quint32, qint16, float)));
+    connect(mLaserScanner, SIGNAL(bottomBeamLength(const float&)), mFlightController, SLOT(slotSetBottomBeamLength(const float&)));
+    connect(mBaseConnection, SIGNAL(enableScanning(const bool&)), mLaserScanner, SLOT(slotEnableScanning(const bool&)));
+    connect(mBaseConnection, SIGNAL(rtkDataReady(const QByteArray&)), mGpsDevice, SLOT(slotSetRtkData(const QByteArray&)));
+
+    //    WARNING! THIS ENABLES MOTION!
+    //    connect(mFlightController, SIGNAL(motion(quint8,qint8,qint8,qint8,qint8)), mKopter, SLOT(slotSetMotion(quint8,qint8,qint8,qint8,qint8)));
+
+    connect(mGpsDevice, SIGNAL(gpsTimeOfWeekEstablished(quint32)), mLaserScanner, SLOT(slotSetScannerTimeStamp(quint32)));
+    connect(mGpsDevice, SIGNAL(newVehiclePose(Pose)), mFlightController, SLOT(slotNewVehiclePose(Pose)));
+    connect(mGpsDevice, SIGNAL(newVehiclePoseLowFreq(Pose)), mBaseConnection, SLOT(slotNewVehiclePose(Pose)));
+
+    connect(mGpsDevice, SIGNAL(newVehiclePoseLowFreq(Pose)), mLaserScanner, SLOT(slotEnableScanning()));
+
+    connect(
+                mGpsDevice,
+                SIGNAL(gpsStatus(const quint8&, const quint8&, const quint16&, const quint8&, const quint8&, const quint8&, const quint8&, const QString&)),
+                mBaseConnection,
+                SLOT(slotNewGpsStatus(const quint8&, const quint8&, const quint16&, const quint8&, const quint8&, const quint8&, const quint8&, const QString&))
+            );
+
+    // Feed sensor data into SensorFuser
+    connect(mGpsDevice, SIGNAL(scanFinished(quint32)), mSensorFuser, SLOT(slotScanFinished(quint32)));
+    connect(mGpsDevice, SIGNAL(newVehiclePose(Pose)), mSensorFuser, SLOT(slotNewVehiclePose(Pose)));
+    connect(mLaserScanner, SIGNAL(newScanData(quint32, std::vector<long>*const)), mSensorFuser, SLOT(slotNewScanData(quint32,std::vector<long>*const)));
+
+    connect(mSensorFuser, SIGNAL(newScannedPoints(QVector3D,QVector<QVector3D>)), mBaseConnection, SLOT(slotNewScannedPoints(QVector3D,QVector<QVector3D>)));
+
+    mTimerComputeMotion = new QTimer(this);
+    mTimerComputeMotion->setInterval(50);
+    mTimerComputeMotion->start();
+    connect(mTimerComputeMotion, SIGNAL(timeout()), mFlightController, SLOT(slotComputeMotionCommands()));
+
+    mKopter->slotSubscribeDebugValues(500);
+
+    //        connect(mKopter, SIGNAL(externControlReplyReceived()), SLOT(slotDoSomething()));
+    //        mKopter->slotSetMotion(fabs(sin(0.00))*40, 0, 0, 0, 0);
+}
+
+KopterControl::~KopterControl()
+{
+    qDebug() << "KopterControl::~KopterControl(): shutting down, deleting objects.";
+    delete mGpsDevice;
+    delete mLaserScanner;
+    delete snSignalPipe;
+
+    // Delete logfiles with a size of 0 (emtpty) or 100 (just ply header, no data)
+    const QFileInfoList list = QDir().entryInfoList((QStringList() << "scannerdata-*" << "pointcloud-*"), QDir::Files | QDir::NoSymLinks);
+    for(int i = 0; i < list.size(); ++i)
+    {
+        const QFileInfo fileInfo = list.at(i);
+        if(fileInfo.size() == 0 || fileInfo.size() == 100)
+        {
+            qDebug() << "LaserScanner::~LaserScanner(): moving useless logfile to /tmp:" << fileInfo.fileName();
+            QFile::rename(fileInfo.canonicalFilePath(), fileInfo.fileName().prepend("/tmp/"));
+        }
+    }
+}
+
+void KopterControl::slotDoSomething()
+{
+    //    QList<unsigned char> speeds;
+    //    speeds << 0 << 3 << 0 << 0 << 0 << 0 << 0 << 0 << 0 << 0 << 0 << 0 << 0 << 0 << 0 << 0;
+    //    mKopter->slotSendMotorTest(speeds);
+
+    static float wert = 0.0;
+    wert += 0.02;
+
+    //    mKopter->slotSetMotion(100, 0, 20, 0, 10);
+    //qDebug() << "setting thrust to" << fabs(sin(wert)*40);
+    qDebug() << "extern control reply received.";
+    //    mKopter->slotSetMotion(fabs(sin(wert))*40, 0, 0, 0, 0);
+}
+
+void KopterControl::signalHandler(int signal)
+{
+    char a = 1;
+    qDebug() << "KopterControl::signalHandler(): received signal" << signal;
+    ::write(signalFd[0], &a, sizeof(a));
+}
+
+void KopterControl::slotHandleSignal()
+{
+    snSignalPipe->setEnabled(false);
+    char tmp;
+    ::read(signalFd[1], &tmp, sizeof(tmp));
+
+    qDebug() << "KopterControl::slotHandleSignal(): caught unix-signal, quitting...";
+
+    snSignalPipe->setEnabled(true);
+
+    // shutdown orderly
+    mLaserScanner->slotEnableScanning(false);
+    mGpsDevice->slotShutDown();
+
+//    quit();
+}
+*/
+
+int main(int argc, char **argv)
+{
+    QCoreApplication a(argc, argv);
+
+    const QStringList arguments = QCoreApplication::arguments();
+
+    if(arguments.size() != 2)
+    {
+        qDebug() << "main(): please specify a logfile for replaying sensor fusion as the only argument.";
+        return 0;
+    }
+
+    LaserScanner* mLaserScanner = new LaserScanner(
+                QString("/does/not/exist"),
+                Pose(
+                    QVector3D(      // Offset from Antenna to Laser Source. In Vehicle Reference Frame: Like OpenGL, red arm forward pointing to screen
+                        +0.09,      // From antenna positive is right to laser
+                        -0.38,      // From Antenna negative is down to laser
+                        -0.11),     // From Antenna negative forward to laser
+                    +000.0,         // No yawing
+                    -090.0,         // 90 deg pitched down
+                    +000.0,         // No rolling
+                    10             // Use 10 msec TOW, so that the relative pose is always older than whatever new pose coming in. Don't use 0, as that would be set to current TOW, which might be newer due to clock offsets.
+                    )
+                );
+
+    // Otherwise, SensorFuser won't accept poses.
+    mLaserScanner->slotEnableScanning(true);
+
+    SensorFuser* mSensorFuser = new SensorFuser(mLaserScanner);
+
+    mSensorFuser->processLog(arguments.last());
+
+    return 0;
+}
