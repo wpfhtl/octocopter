061e92d64d5af87585d1ebbaee13f74e7b47b8dc
ben <ben@f4953187-ecf7-0310-90f3-8d0fedb0aae5>
2011-12-02 15:57:22 +0000

    added logplayer.h
    git-svn-id: https://politbuero.dyndns.org/svn/repos/promotion@1180 f4953187-ecf7-0310-90f3-8d0fedb0aae5

---
+++ koptercontrol/logplayer.h
@@ -0,0 +1,29 @@
+#ifndef LOGPLAYER_H
+#define LOGPLAYER_H
+
+#include <QtCore>
+#include "laserscanner.h"
+#include "sensorfuser.h"
+#include <plymanager.h>
+
+class LogPlayer : public QCoreApplication
+{
+    Q_OBJECT
+
+private:
+    LaserScanner* mLaserScanner;
+    SensorFuser* mSensorFuser;
+    PlyManager* mPlyManager;
+
+public:
+    LogPlayer(int argc, char **argv);
+    ~LogPlayer();
+
+public slots:
+    bool slotProcessLogLine(const QString& line);
+    bool slotProcessLog(const QString& fileName);
+
+signals:
+};
+
+#endif
