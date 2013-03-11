e7da0c53e3664ecd92f185e8c80abf1b0e7b5199
Ben Adler <adler@informatik.uni-hamburg.de>
2013-03-11 20:08:59 +0100

    filewriter works now

--- filewriter/filewriter.h
+++ filewriter/filewriter.h
@@ -1,9 +1,13 @@
 #ifndef FILEWRITER_H
 #define FILEWRITER_H
 
+#include <sys/socket.h>
+#include <signal.h>
+
 #include "filetowrite.h"
 
 #include <QCoreApplication>
+#include <QSocketNotifier>
 #include <QVector>
 #include <QDebug>
 #include <QStringList>
@@ -12,14 +16,23 @@ class FileWriter : public QCoreApplication
 {
     Q_OBJECT
 
+    QVector<FileToWrite*> mFileVector;
+    void printUsage();
+
+    QSocketNotifier *snSignalPipe;
+
+    // We use this pipe for all signals.
+    static int signalFd[2];
+
+public slots:
+    void slotHandleSignal();
+
 public:
     FileWriter(int argc, char **argv);
     ~FileWriter(void);
 
-private:
-    QVector<FileToWrite*> mFileVector;
-    void printUsage();
-
+    // Unix signal handler
+    static void signalHandler(int unused);
 };
 
 #endif
