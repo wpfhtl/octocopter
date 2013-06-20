8c3c8e8d1b8d03b5b7ef886f1500f061a41c04c5
Ben Adler <benadler@gmx.net>
2013-06-20 21:38:25 +0200

    hokuyoid prigram to get device's id, for udev

---
+++ hokuyoid/main.cpp
@@ -0,0 +1,44 @@
+#include "urg/UrgCtrl.h"
+#include <cstdlib>
+#include <iostream>
+
+using namespace qrk;
+using namespace std;
+
+
+int main(int argc, char *argv[])
+{
+    if(argc != 2)
+    {
+        cout << "usage: " << argv[0] << " /dev/ttyACM?" << endl;
+        exit(1);
+    }
+
+    //const char device[] = "COM3";
+    const char device[] = "/dev/ttyACM0";
+
+    UrgCtrl urg;
+    if (!urg.connect(argv[1]))
+    {
+        cerr << "error: connect():" << urg.what();
+        exit(1);
+    }
+
+    vector<string> lines;
+
+    // Receive version information
+    urg.versionLines(lines);
+    if(lines.empty())
+    {
+        cerr << "UrgCtrl::versionLines: " << urg.what() << endl;
+        exit(1);
+    }
+
+    // Output
+    for (vector<string>::iterator it = lines.begin(); it != lines.end(); ++it)
+    {
+        cout << *it << endl;
+    }
+
+    return 0;
+}
